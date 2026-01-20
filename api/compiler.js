b66957b325295e0c6aca2f30d8e33c8ea7177253
Saad Mehmood <hmehmood180@gmail.com>
2026-01-20 20:50:45 +0500

    added comments

--- api/compiler.js
+++ api/compiler.js
@@ -2,20 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 exports.generateFile = (format, content) => {
-    // 1. Generate a unique ID (e.g., timestamp + random number)
+    // 1. Generate a unique ID (timestamp - random number)
     const jobId = Date.now()-Math.random();
-
-    // 2. create the filename (e.g., "12345.cpp")
+    // 2. create the filename 
     const filename = `${jobId}.${format}`; // 1768.cpp
-
     // 3. Construct the full path using path.join()
-    // HINT: Point to the 'temp' folder outside the 'api' folder
-    
     const filePath = path.join(__dirname, '..', 'temp', filename);
-        // TODO: Your Logic Here
-
     // 4. Write the file to disk
-    // TODO: Use fs.writeFileSync
     fs.writeFileSync(filePath, content);
 
     return filePath;

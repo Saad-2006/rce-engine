ce068008eaad2d6c9cb50722773a6bd652641201
Saad Mehmood <hmehmood180@gmail.com>
2026-01-22 19:41:16 +0500

    ticket-3: implemented automated garbage collection

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { generateFile, executeCpp } = require('./api/compiler'); // Import your function
-
+const fs = require('fs');
+const path = require('path');
 const app = express();
 app.use(bodyParser.json());
 
@@ -11,15 +12,24 @@ app.post('/run', async(req, res) => {
     if (!code) {
         return res.status(400).json({ success: false, error: "Empty code body!" });
     }
-
+    let filePath;
+    let outPath;
     try {
-
-        const filePath = generateFile(language, code);
-        
+        filePath = generateFile(language, code);
+        const jobID=path.basename(filePath).split('.')[0];
+        outPath=path.join(path.dirname(filePath), `${jobID}.out`);
         const output = await executeCpp(filePath);
         res.json({ filePath, output});
     } catch (err) {
         res.status(500).json({ success: false, error: err.message });
+    }
+    finally{
+        if(fs.existsSync(filePath)){
+            fs.unlinkSync(filePath);
+        }
+        if(fs.existsSync(outPath)){
+            fs.unlinkSync(outPath);
+        }
     }
 });
 

a52decaedf3d54f7beaf0bd594db1eba62f94a00
Saad Mehmood <hmehmood180@gmail.com>
2026-01-24 14:33:25 +0500

    dockerization completed to avoid inconveniences like rm-rf

--- client/script.js
+++ client/script.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
         outputBox.style.color = "#d4d4d4";
 
         try {
-            const response = await fetch('http://127.0.0.1:5050/run', {
+            const response = await fetch('/run', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ language, code })
